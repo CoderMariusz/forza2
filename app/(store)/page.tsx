d196e23e79896c0885963b2ff871928bf9b5c2ee
Mariusz Krawczyk <przyslony@gmail.com>
2024-12-15 14:35:18 +0000

    remove usier from page

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,10 +1,6 @@
 import { Button } from '@/components/ui/button';
 
-interface User {
-  userHandle: string;
-}
-
-export default function Home(user: User) {
+export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <h1 className='text-4xl font-bold text-center'>Forza Dashboard</h1>
@@ -13,7 +9,7 @@ export default function Home(user: User) {
           <h2 className='text-2xl font-bold'>Welcome!</h2>
           <p>
             This is a dashboard for Forza. It is a project created by Mariusz
-            Krawczyk. hi, {user.userHandle}
+            Krawczyk. hi,
           </p>
           <Button>Get Started</Button>
         </div>
